f439a281cd551a8b2204b9cb361195427555bbbb
sai3010 <saiprasanth2007@gmail.com>
2020-12-11 13:44:42 +0530

    error handled

--- slots/slots/views.py
+++ slots/slots/views.py
@@ -5,14 +5,24 @@ import json
 from .process import *
 @api_view(['POST'])
 def slot_values(request):
-    body_unicode = request.body.decode('utf-8')
-    content = json.loads(body_unicode)
-    resp = validate_finite_values_entity(content['values'],content['supported_values'],content['invalid_trigger'],content['key'],content['support_multiple'],content['pick_first'])
-    return Response(resp)
+    try:
+        body_unicode = request.body.decode('utf-8')
+        content = json.loads(body_unicode)
+        resp = validate_finite_values_entity(content['values'],content['supported_values'],content['invalid_trigger'],content['key'],content['support_multiple'],content['pick_first'])
+        return Response(resp)
+    except KeyError as k:
+        return Response({"error":str(k),"message":"Key Error"},status=500)
+    except Exception as e:
+        return Response({"error":str(e),"message":"Server Error"},status=500)
 
 @api_view(['POST'])
 def slot_numeric(request):
-    body_unicode = request.body.decode('utf-8')
-    content = json.loads(body_unicode)
-    resp = validate_numeric_entity(content['values'],content['invalid_trigger'],content['key'],content['support_multiple'],content['pick_first'],content['constraint'],content['var_name'])
-    return Response(resp)
+    try:
+        body_unicode = request.body.decode('utf-8')
+        content = json.loads(body_unicode)
+        resp = validate_numeric_entity(content['values'],content['invalid_trigger'],content['key'],content['support_multiple'],content['pick_first'],content['constraint'],content['var_name'])
+        return Response(resp)
+    except KeyError as k:
+        return Response({"error":str(k),"message":"Key Error"},status=500)
+    except Exception as e:
+        return Response({"error":str(e),"message":"Server Error"},status=500)
